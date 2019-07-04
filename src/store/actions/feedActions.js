b17b32a16a1b99f21ac4ac23b70e50be9c62afc3
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-04 11:45:25 +0200

    update the readme

--- src/store/actions/feedActions.js
+++ src/store/actions/feedActions.js
@@ -1,48 +1,47 @@
-import axios from "axios";
+import axios from 'axios';
 
-export const setFeedToState = feed => {
-  return {
-    type: "getFeed",
-    payload: feed
-  };
+export const setFeedToState = (feed) => {
+	return {
+		type: 'getFeed',
+		payload: feed
+	};
 };
 
-export const addBlitz = blitz => (dispatch, getState) => {
-  const token = getState().authorizationReducer.token;
-  const body = JSON.stringify(blitz)
+export const addBlitz = (blitz) => (dispatch, getState) => {
+	const token = getState().authorizationReducer.token;
+	const body = JSON.stringify(blitz);
 
+	const headers = new Headers({
+		Authorization: `Bearer ${token}`,
+		'Content-Type': 'application/json'
+	});
 
-  const headers = new Headers({
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json"
-  });
+	const config = {
+		method: 'POST',
+		headers,
+		body
+	};
 
-  const config = {
-    method: "POST",
-    headers,
-    body
-  };
-
-  return fetch("https://propulsion-blitz.herokuapp.com/api/blitzs", config)
-    .then(response => {
-      console.log(response);
-    })
-    .catch(err => err);
+	return fetch('https://propulsion-blitz.herokuapp.com/api/blitzs', config)
+		.then((response) => {
+			console.log(response);
+		})
+		.catch((err) => err);
 };
 
 export const getFeed = () => (dispatch, getState) => {
-  const token = getState().authorizationReducer.token;
-    
-  const config = {
-    headers: {
-      Authorization: `Bearer ${token}`
-    }
-  };
+	const token = getState().authorizationReducer.token;
 
-  axios
-    .get("https://propulsion-blitz.herokuapp.com/api/feed", config)
-    .then(res => {
-      dispatch(setFeedToState(res.data));
-    })
-    .catch(err => err);
+	const config = {
+		headers: {
+			Authorization: `Bearer ${token}`
+		}
+	};
+
+	axios
+		.get('https://propulsion-blitz.herokuapp.com/api/feed', config)
+		.then((res) => {
+			dispatch(setFeedToState(res.data));
+		})
+		.catch((err) => err);
 };
