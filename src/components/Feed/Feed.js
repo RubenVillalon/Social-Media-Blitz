60d2eb54309b180e92adfad027ca891f1efa6f50
Ruben Villalon <rubenvillalond@gmail.com>
2019-07-04 11:37:01 +0200

    updates in the project

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-import { getFeed } from "../../store/actions/feedActions";
+import { getFeed } from '../../store/actions/feedActions';
 
-import Post from "../Post/Post";
+import Post from '../Post/Post';
 
-import "./Feed.css";
+import './Feed.css';
 
-import NewBlitz from "../NewBlitz/NewBlitz";
+import NewBlitz from '../NewBlitz/NewBlitz';
 
 class Feed extends Component {
-  componentDidMount() {
-    this.props.dispatch(getFeed());
-  }
+	componentDidMount() {
+		this.props.dispatch(getFeed());
+	}
 
-  renderFeed = () => {
-    let feed;
-    this.props.feed
-      ? (feed = this.props.feed.reverse().map(feed => {
-          return (
-            <div key={feed._id}>
-              <Post feed={feed} />
-            </div>
-          );
-        }))
-      : (feed = "Loading feed");
-    return feed;
-  };
+	renderFeed = () => {
+		let feed;
+		this.props.feed
+			? (feed = this.props.feed.reverse().map((feed) => {
+					return (
+						<div key={feed._id}>
+							<Post feed={feed} />
+						</div>
+					);
+				}))
+			: (feed = 'Loading feed');
+		return feed;
+	};
 
-  render() {
-    return (
-      <div className="feed-main-container">
-        <NewBlitz />
-        {this.renderFeed()}
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="feed-main-container">
+				<NewBlitz />
+				{this.renderFeed()}
+			</div>
+		);
+	}
 }
-const mapStateToProp = state => {
-  return {
-    feed: state.feedReducer.feed
-  };
+const mapStateToProp = (state) => {
+	return {
+		feed: state.feedReducer.feed
+	};
 };
 export default connect(mapStateToProp)(Feed);
